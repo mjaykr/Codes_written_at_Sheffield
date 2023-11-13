f710986a2bc6df64d4781c7884ea40dbb05b4b41
Mirtunjay Kumar <mjay@hotmail.com>
2023-11-13 13:06:32 +0000

    Commented the deletion command by default
    Commented the deletion command for _processed.xlsx files

--- Alemnis_Data_analysis/Analyzing_all_excel_files_in_folder.m
+++ Alemnis_Data_analysis/Analyzing_all_excel_files_in_folder.m
@@ -279,11 +279,11 @@ end
 %%%%%%%% Deleting _processed.xlsx Files %%%%%%%%%%
 % % Get a list of all files in the current directory with names that end with '_processed.xlsx'
 
-files = dir('*_processed.xlsx');
-% Loop over the files and delete each one
-for i = 1:length(files)
-    delete(files(i).name);
-end
+% files = dir('*_processed.xlsx');
+% % Loop over the files and delete each one
+% for i = 1:length(files)
+%     delete(files(i).name);
+% end
 
 %%%%%%%% Deleting *.png Files %%%%%%%%%%
 % Get a list of all files in the current directory with names that end with '_processed.xlsx'
